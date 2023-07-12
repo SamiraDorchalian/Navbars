ebe2441995f29aa3098f7f435bfb94ad163b5565
samira dorchalian <samiradorchalain1998@gmail.com>
2023-07-12 14:35:40 +0430

    Practice Navbar-10

---
+++ Navbar-10/script.js
@@ -0,0 +1,5 @@
+$('#header').prepend('<div id="menu-icon"><span class="first"></span><span class="second"></span><span class="third"></span></div>');
+    $("#menu-icon").on("click", function(){
+    $("nav").slideToggle();
+    $(this).toggleClass("active");
+});
